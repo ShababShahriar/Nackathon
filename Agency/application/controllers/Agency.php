2e848fd769643212d8d07478cc0ead9267e55f7f
Md. Saiful Islam <saiful.11722@gmail.com>
2016-04-06 16:46:08 +0600

    Agent Module Added, Database updated

---
+++ Agency/application/controllers/Agency.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+*	LAST MODIFIED : 29-06-2015 04:02 PM
+*/
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Agency extends CI_Controller {
+	  
+	 public function __construct()
+     {
+          parent::__construct();
+		  
+		  /**
+		  *	Load Libraries , Models and Helpers
+		  */
+		  
+          $this->load->library('session');
+          $this->load->helper('form');
+          $this->load->helper('url');
+          $this->load->helper('html');
+		  $this->load->library('form_validation');
+		  
+		if(!isset($_SESSION["agencyName"]))
+		{
+			redirect('/home', 'refresh');
+		}
+		  
+		//load models
+		
+		$this->load->view('templates/header2');
+     }
+	 
+	/**
+	*	[ADMIN HOME PAGE]
+	*/
+	
+	public function index()			
+	{
+		echo "Hello".$_SESSION['agencyName'];
+		//redirect('admin/search','refresh');
+	}
+	
+	/**
+	*	Admin Logs out
+	*/
+	
+	public function logout()	
+	{
+		$this->session->sess_destroy();	//!Stop Session 
+		
+		/**
+		*Redirect To Homepage
+		*/
+		
+		redirect('/home', 'refresh');
+	}
+	
+	public function test()
+	{
+		echo "OK";
+	}
+}
--- Database/nakathon_2.sql
+++ Database/nakathon_2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2016 at 11:25 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Apr 06, 2016 at 12:41 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,10 +27,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `userName` varchar(127) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `userName`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -39,11 +46,11 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 CREATE TABLE IF NOT EXISTS `agency` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `agencyName` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `descriptionId` int(11) DEFAULT NULL,
+  `description` varchar(8192) DEFAULT NULL,
   `rating` double DEFAULT NULL,
   `websiteLink` varchar(1023) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
@@ -52,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `agency` (
 -- Dumping data for table `agency`
 --
 
-INSERT INTO `agency` (`id`, `email`, `agencyName`, `password`, `descriptionId`, `rating`, `websiteLink`) VALUES
+INSERT INTO `agency` (`id`, `email`, `agencyName`, `password`, `description`, `rating`, `websiteLink`) VALUES
 (1, NULL, 'agency1', '3bfb04fca479ac4a8d4ddd4b02868fd0', NULL, NULL, NULL),
 (2, NULL, 'agency2', '7babfa807a160064a31b52c1cb25054b', NULL, NULL, NULL);
 
@@ -63,7 +70,7 @@ INSERT INTO `agency` (`id`, `email`, `agencyName`, `password`, `descriptionId`, 
 --
 
 CREATE TABLE IF NOT EXISTS `agency_hotel` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `agencyId` int(11) NOT NULL,
   `hotelId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -75,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `agency_hotel` (
 --
 
 CREATE TABLE IF NOT EXISTS `agency_offer` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `agencyId` int(11) NOT NULL,
   `description` varchar(1023) DEFAULT NULL,
   `startingDate` datetime DEFAULT NULL,
@@ -89,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `agency_offer` (
 --
 
 CREATE TABLE IF NOT EXISTS `agency_post` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `agencyId` int(11) NOT NULL,
   `text` varchar(1023) DEFAULT NULL,
   `time` datetime DEFAULT NULL
@@ -102,7 +109,7 @@ CREATE TABLE IF NOT EXISTS `agency_post` (
 --
 
 CREATE TABLE IF NOT EXISTS `agency_spot` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `agencyId` int(11) NOT NULL,
   `spotId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -114,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `agency_spot` (
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `spotId` int(11) NOT NULL,
   `cat` int(11) NOT NULL COMMENT '0: mountains, 1: waterfall, 2: forest, 3: lake, 4: sea beach'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -126,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `category` (
 --
 
 CREATE TABLE IF NOT EXISTS `contact` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `entityId` int(11) NOT NULL,
   `details` varchar(255) DEFAULT NULL COMMENT 'a single mode of contact (i.e. e-mail id or cellphone no)',
   `type` int(11) NOT NULL COMMENT '0: agency, 1: hotel, 2: guide, 3: food shop, 4: police number, 5: fire service number'
@@ -139,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `contact` (
 --
 
 CREATE TABLE IF NOT EXISTS `description` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `type` int(11) NOT NULL COMMENT '0: agency, 1: hotel, 2: guide, 3: spot',
   `entityId` int(11) NOT NULL,
@@ -154,7 +161,7 @@ CREATE TABLE IF NOT EXISTS `description` (
 --
 
 CREATE TABLE IF NOT EXISTS `estimated_cost` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `type` int(11) NOT NULL COMMENT '0: spot, 1: guide',
   `spotId` int(11) DEFAULT NULL COMMENT 'if cost is for guide then add the spot id for which the guide is charging this amount',
@@ -169,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `estimated_cost` (
 --
 
 CREATE TABLE IF NOT EXISTS `guide` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(127) DEFAULT NULL,
   `costId` int(11) DEFAULT NULL,
   `descriptionId` int(11) DEFAULT NULL
@@ -182,7 +189,7 @@ CREATE TABLE IF NOT EXISTS `guide` (
 --
 
 CREATE TABLE IF NOT EXISTS `guide_language` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `guideId` int(11) NOT NULL,
   `language` varchar(127) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -194,7 +201,7 @@ CREATE TABLE IF NOT EXISTS `guide_language` (
 --
 
 CREATE TABLE IF NOT EXISTS `guide_spot` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `guideId` int(11) NOT NULL,
   `spotId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -206,7 +213,7 @@ CREATE TABLE IF NOT EXISTS `guide_spot` (
 --
 
 CREATE TABLE IF NOT EXISTS `hotel` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(127) NOT NULL,
   `locationDesc` varchar(1023) DEFAULT NULL COMMENT 'informal description of location',
   `locationId` int(11) DEFAULT NULL,
@@ -222,7 +229,7 @@ CREATE TABLE IF NOT EXISTS `hotel` (
 --
 
 CREATE TABLE IF NOT EXISTS `hotel_offer` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `hotelId` int(11) NOT NULL,
   `description` varchar(1023) DEFAULT NULL,
   `startingDate` datetime DEFAULT NULL,
@@ -236,7 +243,7 @@ CREATE TABLE IF NOT EXISTS `hotel_offer` (
 --
 
 CREATE TABLE IF NOT EXISTS `image` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `entityId` int(11) NOT NULL COMMENT 'hotel, spot or agency post id',
   `img` blob,
   `time` datetime DEFAULT NULL,
@@ -250,7 +257,7 @@ CREATE TABLE IF NOT EXISTS `image` (
 --
 
 CREATE TABLE IF NOT EXISTS `location` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `googleId` varchar(255) NOT NULL,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
@@ -266,7 +273,7 @@ CREATE TABLE IF NOT EXISTS `location` (
 --
 
 CREATE TABLE IF NOT EXISTS `rating` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `type` int(11) NOT NULL COMMENT '0: agency, 1: hotel, 2: guide, 3: spot, 4: description, 5: estimated cost, 6: security',
   `entityId` int(11) NOT NULL,
@@ -281,7 +288,7 @@ CREATE TABLE IF NOT EXISTS `rating` (
 --
 
 CREATE TABLE IF NOT EXISTS `security` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `spotId` int(11) NOT NULL,
   `userId` int(11) DEFAULT NULL,
   `time` datetime DEFAULT NULL,
@@ -295,7 +302,7 @@ CREATE TABLE IF NOT EXISTS `security` (
 --
 
 CREATE TABLE IF NOT EXISTS `spot` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(127) DEFAULT NULL,
   `locationId` int(11) NOT NULL,
   `descriptionId` int(11) DEFAULT NULL,
@@ -312,7 +319,7 @@ CREATE TABLE IF NOT EXISTS `spot` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`userId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `userName` varchar(127) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -328,7 +335,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 CREATE TABLE IF NOT EXISTS `video` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `userId` int(11) DEFAULT NULL,
   `spotId` int(11) DEFAULT NULL,
   `time` datetime DEFAULT NULL,
@@ -342,7 +349,7 @@ CREATE TABLE IF NOT EXISTS `video` (
 --
 
 CREATE TABLE IF NOT EXISTS `visited_spots` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `spotId` int(11) NOT NULL,
   `date` datetime DEFAULT NULL
@@ -356,139 +363,139 @@ CREATE TABLE IF NOT EXISTS `visited_spots` (
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `agency`
 --
 ALTER TABLE `agency`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`), ADD KEY `descriptionId` (`descriptionId`), ADD KEY `rating` (`rating`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`), ADD KEY `descriptionId` (`description`(767)), ADD KEY `rating` (`rating`);
 
 --
 -- Indexes for table `agency_hotel`
 --
 ALTER TABLE `agency_hotel`
- ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`), ADD KEY `hotelId` (`hotelId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`), ADD KEY `hotelId` (`hotelId`);
 
 --
 -- Indexes for table `agency_offer`
 --
 ALTER TABLE `agency_offer`
- ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`);
 
 --
 -- Indexes for table `agency_post`
 --
 ALTER TABLE `agency_post`
- ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`);
 
 --
 -- Indexes for table `agency_spot`
 --
 ALTER TABLE `agency_spot`
- ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`), ADD KEY `spotId` (`spotId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `agencyId` (`agencyId`), ADD KEY `spotId` (`spotId`);
 
 --
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `contact`
 --
 ALTER TABLE `contact`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `description`
 --
 ALTER TABLE `description`
- ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`);
 
 --
 -- Indexes for table `estimated_cost`
 --
 ALTER TABLE `estimated_cost`
- ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`), ADD KEY `spotId` (`spotId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`), ADD KEY `spotId` (`spotId`);
 
 --
 -- Indexes for table `guide`
 --
 ALTER TABLE `guide`
- ADD PRIMARY KEY (`id`), ADD KEY `costId` (`costId`), ADD KEY `descriptionId` (`descriptionId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `costId` (`costId`), ADD KEY `descriptionId` (`descriptionId`);
 
 --
 -- Indexes for table `guide_language`
 --
 ALTER TABLE `guide_language`
- ADD PRIMARY KEY (`id`), ADD KEY `guideId` (`guideId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `guideId` (`guideId`);
 
 --
 -- Indexes for table `guide_spot`
 --
 ALTER TABLE `guide_spot`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `hotel`
 --
 ALTER TABLE `hotel`
- ADD PRIMARY KEY (`id`), ADD KEY `descriptionId` (`descriptionId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `descriptionId` (`descriptionId`);
 
 --
 -- Indexes for table `hotel_offer`
 --
 ALTER TABLE `hotel_offer`
- ADD PRIMARY KEY (`id`), ADD KEY `hotelId` (`hotelId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `hotelId` (`hotelId`);
 
 --
 -- Indexes for table `image`
 --
 ALTER TABLE `image`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `location`
 --
 ALTER TABLE `location`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `rating`
 --
 ALTER TABLE `rating`
- ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`);
 
 --
 -- Indexes for table `security`
 --
 ALTER TABLE `security`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `spot`
 --
 ALTER TABLE `spot`
- ADD PRIMARY KEY (`id`), ADD KEY `descriptionId` (`descriptionId`), ADD KEY `securityId` (`securityId`), ADD KEY `estimatedCostId` (`estimatedCostId`), ADD KEY `locationId` (`locationId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `descriptionId` (`descriptionId`), ADD KEY `securityId` (`securityId`), ADD KEY `estimatedCostId` (`estimatedCostId`), ADD KEY `locationId` (`locationId`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`userId`), ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`userId`), ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `video`
 --
 ALTER TABLE `video`
- ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`), ADD KEY `spotId` (`spotId`);
+  ADD PRIMARY KEY (`id`), ADD KEY `userId` (`userId`), ADD KEY `spotId` (`spotId`);
 
 --
 -- Indexes for table `visited_spots`
 --
 ALTER TABLE `visited_spots`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -498,126 +505,120 @@ ALTER TABLE `visited_spots`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `agency`
 --
 ALTER TABLE `agency`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `agency_hotel`
 --
 ALTER TABLE `agency_hotel`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `agency_offer`
 --
 ALTER TABLE `agency_offer`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `agency_post`
 --
 ALTER TABLE `agency_post`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `agency_spot`
 --
 ALTER TABLE `agency_spot`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `description`
 --
 ALTER TABLE `description`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `estimated_cost`
 --
 ALTER TABLE `estimated_cost`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `guide`
 --
 ALTER TABLE `guide`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `guide_language`
 --
 ALTER TABLE `guide_language`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `guide_spot`
 --
 ALTER TABLE `guide_spot`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `hotel`
 --
 ALTER TABLE `hotel`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `hotel_offer`
 --
 ALTER TABLE `hotel_offer`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `image`
 --
 ALTER TABLE `image`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `location`
 --
 ALTER TABLE `location`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `rating`
 --
 ALTER TABLE `rating`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `security`
 --
 ALTER TABLE `security`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `spot`
 --
 ALTER TABLE `spot`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `video`
 --
 ALTER TABLE `video`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `visited_spots`
 --
 ALTER TABLE `visited_spots`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `agency`
---
-ALTER TABLE `agency`
-ADD CONSTRAINT `agency_ibfk_1` FOREIGN KEY (`descriptionId`) REFERENCES `description` (`id`);
 
 --
 -- Constraints for table `agency_hotel`
